0090b215952215d50f8341bc27f30af0900b0aa9
Shezard <dam.fayol@gmail.com>
2020-08-26 20:23:59 +0200

    Use https

--- src/Result.js
+++ src/Result.js
@@ -10,19 +10,19 @@ function Result(props) {
                 <SetIcon set={props.result.set} /> {props.result.set} - {props.result.player} - <ColorIcon colors={props.result.colors} />
             </h2>
             <div className="links">
-                <a href={`http://youtu.be/${props.result.video_id}?t=${props.result.timestamp_draft}`}>Draft start</a>
+                <a href={`https://youtu.be/${props.result.video_id}?t=${props.result.timestamp_draft}`}>Draft start</a>
                 {
                     props.result.timestamp_match &&
                         <>
                             <span> - </span>
-                            <a href={`http://youtu.be/${props.result.video_id}?t=${props.result.timestamp_match}`}>Match start</a>
+                            <a href={`https://youtu.be/${props.result.video_id}?t=${props.result.timestamp_match}`}>Match start</a>
                         </>
                 }
             </div>
             <div className="video">
                 <i className="spinner fa fa-spinner-notch fa-spin"></i>
                     <iframe
-                        src={`http://www.youtube.com/embed/${props.result.video_id}?start=${props.result.timestamp_draft}`}
+                        src={`https://www.youtube.com/embed/${props.result.video_id}?start=${props.result.timestamp_draft}`}
                         width="720"
                         height="360"
                         title="View draft"
