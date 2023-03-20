71e0a71e2c85eaa4b3055d52464f5148b2af3cb5
unknown <dam.fayol@gmail.com>
2023-03-20 20:00:48 +0100

    Add missing files

---
+++ docs/precache-manifest.c9c4a168a5cfd8e34e5f986deec1c5ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3958197baa51024c03034c5b47868a1",
+    "url": "/learnlimited/index.html"
+  },
+  {
+    "revision": "b8b4bae1c66466d5e756",
+    "url": "/learnlimited/static/css/main.631fe87a.chunk.css"
+  },
+  {
+    "revision": "e502f172587eeaa4fcb2",
+    "url": "/learnlimited/static/js/2.e0fc6947.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learnlimited/static/js/2.e0fc6947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b4bae1c66466d5e756",
+    "url": "/learnlimited/static/js/main.ead1c714.chunk.js"
+  },
+  {
+    "revision": "e42dc9b0b42d4f7e5343",
+    "url": "/learnlimited/static/js/runtime-main.9069f9e8.js"
+  }
+]);
