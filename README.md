18f421711f105f0eb7180788b1fc83bbf9aed3e1
unknown <dam.fayol@gmail.com>
2023-03-20 19:37:40 +0100

    Build project inside a docs folder for github pages

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Learn Limited
+
+This project is a website where you can search for MTG limited youtube matches
 
 ## Available Scripts
 
---
+++ docs/precache-manifest.6cda8a27461ab0c06c083ffce56843d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f831778eff76c6a65ab8e607f5a6ef",
+    "url": "/learnlimited/index.html"
+  },
+  {
+    "revision": "697f6a4ddc796f44fbb2",
+    "url": "/learnlimited/static/css/main.631fe87a.chunk.css"
+  },
+  {
+    "revision": "e502f172587eeaa4fcb2",
+    "url": "/learnlimited/static/js/2.e0fc6947.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learnlimited/static/js/2.e0fc6947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "697f6a4ddc796f44fbb2",
+    "url": "/learnlimited/static/js/main.18302497.chunk.js"
+  },
+  {
+    "revision": "e42dc9b0b42d4f7e5343",
+    "url": "/learnlimited/static/js/runtime-main.9069f9e8.js"
+  }
+]);
